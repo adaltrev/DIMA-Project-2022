42e0a38045c3abe1ef6cf60d178ddde9048c390a
liviobi <10546479@polimi.it>
2022-04-29 21:49:32 +0200

    fixed ui overflow

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,6 +3,7 @@ import 'package:dima_project/screens/poster_screen.dart';
 import 'package:dima_project/widgets/seasons_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../model/categories.dart';
 import '../model/serie.dart';
@@ -26,82 +27,68 @@ class _DetailsScreenState extends State<DetailsScreen> {
     return Scaffold(
       appBar: AppBar(title: Text(serie.name)),
       body: SingleChildScrollView(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    width: 20,
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 3.h),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () => {
+                    Navigator.pushNamed(context, PosterScreen.routeName,
+                        arguments: serie.posterPath)
+                  },
+                  icon: CachedNetworkImage(
+                    imageUrl:
+                        "http://image.tmdb.org/t/p/w342" + serie.posterPath,
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
                   ),
-                  IconButton(
-                    onPressed: () => {
-                      Navigator.pushNamed(context, PosterScreen.routeName,
-                          arguments: serie.posterPath)
-                    },
-                    icon: CachedNetworkImage(
-                      height: 300,
-                      width: 200,
-                      imageUrl:
-                          "http://image.tmdb.org/t/p/w342" + serie.posterPath,
-                      placeholder: (context, url) =>
-                          const CircularProgressIndicator(),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    ),
-                    constraints: const BoxConstraints(
-                      minHeight: 300,
-                      minWidth: 200,
-                    ),
+                  constraints: BoxConstraints(
+                    minHeight: 69.w,
+                    minWidth: 46.w,
                   ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(children: [
-                        IconButton(
-                            onPressed: () => {
-                                  Navigator.pushNamed(
-                                      context, AddScreen.routeName,
-                                      arguments: serie)
-                                },
-                            icon: serie.category == Categories.searched
-                                ? const Icon(Icons.add_circle)
-                                : const Icon(Icons.edit_note))
-                      ]),
-                      Text("Genre: " + serie.genre),
-                      Text("Country: " + serie.country),
-                      Text("Status: " + serie.status),
-                      Text(serie.totalSeasons.toString() +
-                          " seasons, " +
-                          serie.totalEpisodes.toString() +
-                          " episodes"),
-                      Text("First aired: " + serie.beginDate),
-                      Text("Last aired: " + serie.endDate)
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                serie.overview,
-              ),
-              SeasonsPages(serie)
-            ]),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(children: [
+                      IconButton(
+                          onPressed: () => {
+                                Navigator.pushNamed(
+                                    context, AddScreen.routeName,
+                                    arguments: serie)
+                              },
+                          icon: serie.category == Categories.searched
+                              ? const Icon(Icons.add_circle)
+                              : const Icon(Icons.edit_note))
+                    ]),
+                    Text("Genre: " + serie.genre),
+                    Text("Country: " + serie.country),
+                    Text("Status: " + serie.status),
+                    Text(serie.totalSeasons.toString() +
+                        " seasons, " +
+                        serie.totalEpisodes.toString() +
+                        " episodes"),
+                    Text("First aired: " + serie.beginDate),
+                    Text("Last aired: " + serie.endDate)
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(2.w, 0, 1.w, 2.h),
+            child: Text(
+              serie.overview,
+            ),
+          ),
+          SeasonsPages(serie)
+        ]),
       ),
     );
   }
