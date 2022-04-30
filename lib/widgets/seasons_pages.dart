5149cb019d863af8763e7fa4286c773f8fa9cb4a
liviobi <10546479@polimi.it>
2022-04-30 17:28:54 +0200

    added page indicator

--- lib/widgets/seasons_pages.dart
+++ lib/widgets/seasons_pages.dart
@@ -16,8 +16,27 @@ class SeasonsPages extends StatefulWidget {
 
 class _SeasonsPagesState extends State<SeasonsPages> {
   int selectedCard = 0;
+  final int seasonsPerPage = 8;
   //I use this to higligth the season that I'm currently watching when opening the app
   bool firstBuild = true;
+  int currentPage = 0;
+  final PageController controller = PageController();
+
+  getTotalPages() {
+    int totalSeasons = widget.serie.seasons.length;
+    num totalPages = totalSeasons % seasonsPerPage == 0
+        ? totalSeasons / seasonsPerPage
+        : (totalSeasons / seasonsPerPage).floor() + 1;
+    var resultList = [];
+    for (int i = 0; i < totalPages; i++) {
+      resultList.add(i);
+    }
+    return resultList;
+  }
+
+  int getPageToShowIndex(int selectedCard) {
+    return (selectedCard / seasonsPerPage).floor();
+  }
 
   updateSelectedCard(int newSelectedCardIndex) {
     setState(() {
@@ -29,13 +48,21 @@ class _SeasonsPagesState extends State<SeasonsPages> {
     setState(() {
       selectedCard = serie.addEpisodes(selectedCard, quantity);
     });
+
+    if (controller.page! > getPageToShowIndex(selectedCard)) {
+      controller.previousPage(
+          duration: const Duration(milliseconds: 100), curve: Curves.easeIn);
+    } else if (controller.page! < getPageToShowIndex(selectedCard)) {
+      controller.nextPage(
+          duration: const Duration(milliseconds: 100), curve: Curves.easeIn);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController();
     if (firstBuild) {
       updateSelectedCard(widget.serie.currentlyWatchingSeason());
+      currentPage = getPageToShowIndex(selectedCard);
       firstBuild = false;
     }
     return Column(
@@ -43,15 +70,37 @@ class _SeasonsPagesState extends State<SeasonsPages> {
         const Text("Seasons"),
         Container(
           color: Colors.red,
-          height: 400,
+          height: 350,
           width: 500,
-          child: PageView(controller: controller, children: [
-            ...widget.serie.seasonsSublisted(8).map((seasonsSublist) {
-              return SeasonGrid(
-                  seasonsSublist, updateSelectedCard, selectedCard);
-            }),
-          ]),
+          child: PageView(
+              controller: controller,
+              onPageChanged: (newPage) {
+                setState(() {
+                  currentPage = newPage;
+                });
+              },
+              children: [
+                ...widget.serie
+                    .seasonsSublisted(seasonsPerPage)
+                    .map((seasonsSublist) {
+                  return SeasonGrid(
+                      seasonsSublist, updateSelectedCard, selectedCard);
+                }),
+              ]),
         ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ...getTotalPages().map((page) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 0.8.w),
+              child: Icon(
+                Icons.circle,
+                size: 2.w,
+                color: page == currentPage ? Colors.white : Colors.grey,
+              ),
+            );
+          }),
+        ]),
+
         //if I'm searching the series hide the tools to update watching episodes
         if (widget.serie.category != Categories.searched) ...[
           AdderBar(widget.serie, addEpisode, selectedCard),
